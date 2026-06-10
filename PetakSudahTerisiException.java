248c0521e3c9991e6763fc027520c7ff0a65104c
YogaKarang <karangyoga@gmail.com>
2026-06-10 15:15:27 +0800

    Delete PetakSudahTerisiException.java

--- PetakSudahTerisiException.java
+++
@@ -1,6 +0,0 @@
-// Dibuat oleh Mahasiswa 2
-public class PetakSudahTerisiException extends Exception {
-    public PetakSudahTerisiException(int row, int col) {
-        super("Petak ["+ row + "]["+ col + "] sudah terisi!");
-    }
-}
